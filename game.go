888f496dd58921762d9b1a2c8cc8170a8c82967a
marcmaniez <marc.maniez@gmail.com>
2023-06-15 21:37:07 +0200

    Implement moving to the right

--- game.go
+++ game.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 // Game implements ebiten.Game interface and is composed of one active shape that the player controls and needs to place, and a collection of squares that have already been placed
@@ -12,25 +14,34 @@ type Game struct {
 	shape                  *shape
 	squares                []*square
 	frame                  [4]*rectangle
+	lastMove               int
+	throttle               int
 	updates                int
 	interval               int
 	updates_since_movement int
 	shapes                 shapes
 }
 
-func InitShapes() shapes {
-	arr := shapes{}
-	shp := shape{}
-	shp[0] = MakeSquare(color.RGBA{0xff, 0, 0, 0xff}, coords{0, 0})
-	shp[1] = MakeSquare(color.RGBA{0xff, 0, 0, 0xff}, coords{0, 48})
-	shp[2] = MakeSquare(color.RGBA{0xff, 0, 0, 0xff}, coords{0, 2 * 48})
-	shp[3] = MakeSquare(color.RGBA{0xff, 0, 0, 0xff}, coords{0, 3 * 48})
-	return arr
+func initGame() *Game {
+	frame_clr := color.RGBA{184, 184, 184, 0xff}
+	g := Game{
+		interval: 6,
+		throttle: 20,
+		frame: [4]*rectangle{
+			makeRectangle(coords{482, 2}, frame_clr, coords{0, 0}),
+			makeRectangle(coords{2, 962}, frame_clr, coords{482, 0}),
+			makeRectangle(coords{482, 2}, frame_clr, coords{2, 962}),
+			makeRectangle(coords{2, 962}, frame_clr, coords{0, 2}),
+		},
+	}
+	g.spawnShape()
+	return &g
 }
 
 func (g *Game) Update() error {
 	g.updates++
 	g.updates_since_movement++
+	g.HandleInput()
 	if g.updates_since_movement >= g.interval {
 		g.updates_since_movement = 0
 		g.shape.move()
@@ -74,8 +85,11 @@ func (g *Game) MoveLeft() {
 }
 
 func (g *Game) MoveRight() {
-	// TODO: implement me
-	// g.shape.moveRight()
+	for _, square := range *g.shape {
+		for _, rect := range *square {
+			rect.x += 48
+		}
+	}
 }
 
 func (g *Game) MoveDown() {
@@ -86,21 +100,6 @@ func (g *Game) MoveDown() {
 func (g *Game) spawnShape() {
 	index := rand.Intn(7)
 	g.shape = shapeFuncs[index](coords{x: 5 * 48, y: -48 + 2})
-}
-
-func initGame() *Game {
-	frame_clr := color.RGBA{184, 184, 184, 0xff}
-	g := Game{
-		interval: 6,
-		frame: [4]*rectangle{
-			makeRectangle(coords{482, 2}, frame_clr, coords{0, 0}),
-			makeRectangle(coords{2, 962}, frame_clr, coords{482, 0}),
-			makeRectangle(coords{482, 2}, frame_clr, coords{2, 962}),
-			makeRectangle(coords{2, 962}, frame_clr, coords{0, 2}),
-		},
-	}
-	g.spawnShape()
-	return &g
 }
 
 func DrawRectangle(screen *ebiten.Image, rect *rectangle) {
@@ -136,4 +135,26 @@ func (g *Game) TransferShapeToSquares() {
 	for _, sqr := range *g.shape {
 		g.squares = append(g.squares, sqr)
 	}
+}
+
+func (g *Game) HandleInput() {
+	keys := []ebiten.Key{}
+	keys = inpututil.AppendPressedKeys(keys)
+	fmt.Println(keys)
+	// Do nothing if no keys are pressed or if more than one key is pressed,
+	// or if trying to move before the throttle period has passed
+	if len(keys) != 1 || g.updates-g.lastMove < g.throttle {
+		return
+	}
+	switch key := keys[0]; key {
+	case ebiten.KeyArrowUp:
+		g.Rotate()
+	case ebiten.KeyArrowRight:
+		g.MoveRight()
+	case ebiten.KeyArrowDown:
+		g.MoveDown()
+	case ebiten.KeyArrowLeft:
+		g.MoveLeft()
+	}
+	g.lastMove = g.updates
 }
