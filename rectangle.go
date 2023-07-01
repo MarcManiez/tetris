f5aee923a7334f58b027ae204df5362826ea016d
marcmaniez <marc.maniez@gmail.com>
2023-07-01 16:11:45 -0700

    Don't spam rectangles

--- rectangle.go
+++ rectangle.go
@@ -7,6 +7,8 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+var rectangles = make(map[coords]*ebiten.Image)
+
 // rectangle is the smallest unit of UI. All other elements are made up of rectangles.
 type rectangle struct {
 	img *ebiten.Image
@@ -20,11 +22,15 @@ func (r *rectangle) DrawRectangle(screen *ebiten.Image) {
 	op.GeoM.Translate(float64(r.x), float64(r.y))
 	_, _, _, a := r.clr.RGBA()
 	op.ColorScale.ScaleAlpha((float32(uint8(a>>8)) / math.MaxUint8))
+	r.img.Fill(r.clr)
 	screen.DrawImage(r.img, op)
 }
 
 func makeRectangle(size coords, clr color.Color, position coords) *rectangle {
-	img := ebiten.NewImage(size.x, size.y)
-	img.Fill(clr)
+	img := rectangles[size]
+	if img == nil {
+		img = ebiten.NewImage(size.x, size.y)
+		rectangles[size] = img
+	}
 	return &rectangle{img, position.x, position.y, clr}
 }
